58164d409807129b0c849c6e35fd13920b925db3
WebExpert <giorgos@webexpert.gr>
2021-02-26 11:24:15 +0200

    small typo on email

--- resources/views/mails/company_update.blade.php
+++ resources/views/mails/company_update.blade.php
@@ -4,7 +4,7 @@
 	@if($created)
 		{{ __("A new company named") }}  {{ $company->name }} {{ __("has been created successfully.") }}
 	@else
-		{{ __("Someone has been updated company") }} {{ $company->name }}. {{ __("You can take alook") }}
+		{{ __("Someone has been updated company") }} {{ $company->name }}. {{ __("You can take a look") }}
 	@endif
 </p>
 
